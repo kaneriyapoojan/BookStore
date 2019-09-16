65d09fde358ed2e1b65b43cb78ce6a5d5a85ea72
jcfdsilva <jcfdsilva@gmail.com>
2019-09-17 01:45:59 +0300

    week4 Database integration h2

--- w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/domain/Book.java
+++ w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/domain/Book.java
@@ -1,10 +1,27 @@
 package com.example.w2ex4BookStore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
-	private String title, author, year, isbn;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long isbn;
+	private String title, author, year;
 	private double price;
 	
+	public Book() {}
 	
+	public Book(String title, String author, String year, Long isbn, double price) {
+		this.title=title;
+		this.author=author;
+		this.year=year;
+		this.isbn=isbn;
+		this.price=price;
+	}
 	public String getTitle() {
 		return title;
 	}
@@ -23,10 +40,10 @@ public class Book {
 	public void setYear(String year) {
 		this.year = year;
 	}
-	public String getIsbn() {
+	public Long getIsbn() {
 		return isbn;
 	}
-	public void setIsbn(String isbn) {
+	public void setIsbn(Long isbn) {
 		this.isbn = isbn;
 	}
 	public double getPrice() {
@@ -35,5 +52,8 @@ public class Book {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+	@Override
+	public String toString() {
+		return "Book: "+title;
+	}
 }
--- w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/web/BookController.java
+++ w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/web/BookController.java
@@ -1,14 +1,49 @@
 package com.example.w2ex4BookStore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.w2ex4BookStore.domain.Book;
+import com.example.w2ex4BookStore.domain.BookRepository;
 
 @Controller
 public class BookController {
-	@RequestMapping("/index")
-	public String greeting(Model model) {
-		return "";
-		
-	}
+	
+	@Autowired
+	private BookRepository repository; 
+	
+    @RequestMapping(value= {"/", "/booklist"})
+    public String studentList(Model model) {	
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+  
+    @RequestMapping(value = "/add")
+    public String addStudent(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+    
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", repository.findById(bookId));
+        return "editBook";
+    }
+	
 }
