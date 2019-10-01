b3080df85ba758a8d79821d8709b410bcff09f54
jcfdsilva <jcfdsilva@gmail.com>
2019-10-01 10:54:41 +0300

    rest /api

--- w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/web/BookController.java
+++ w2ex4BookStore/src/main/java/com/example/w2ex4BookStore/web/BookController.java
@@ -1,11 +1,15 @@
 package com.example.w2ex4BookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.w2ex4BookStore.domain.Book;
 import com.example.w2ex4BookStore.domain.BookRepository;
@@ -52,4 +56,17 @@ public class BookController {
         return "editBook";
     }
 	
+	// RESTful service to get all students
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+
+	// RESTful service to get student by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long isbn) {	
+    	return repository.findById(isbn);
+    }       
+    
+    
 }
